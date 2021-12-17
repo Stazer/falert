b553783d42406d99240cc3792156d2c3861b140f
Stazer <justus@sutsuj.com>
2021-12-17 15:58:02 +0100

    [#11] Add initialization of database connection and its request injection

---
+++ falert/backend/http/middleware.py
@@ -0,0 +1,30 @@
+from sanic.request import Request
+from sanic.response import HTTPResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlalchemy.orm import sessionmaker
+
+
+class BaseMiddleware:
+    pass
+
+
+class AttachDatabaseMiddleware(BaseMiddleware):
+    def __init__(self, engine: AsyncEngine):
+        self.__engine = engine
+
+    async def __call__(self, request: Request):
+        session_maker = sessionmaker(
+            self.__engine,
+            expire_on_commit=False,
+            class_=AsyncSession,
+        )
+
+        async with session_maker() as session:
+            request.ctx.database = session
+
+
+class DetachDatabaseMiddleware(BaseMiddleware):
+    async def __call__(self, request: Request, _response: HTTPResponse):
+        if hasattr(request.ctx, "database"):
+            await request.ctx.database.close()
