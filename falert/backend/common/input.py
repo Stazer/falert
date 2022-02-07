46afc77847244798b719469f4a3a25750be97741
Justus Flerlage <justus@sutsuj.com>
2022-02-08 00:27:15 +0100

    [#42] Add NASA fire locations input model

--- falert/backend/common/input.py
+++ falert/backend/common/input.py
@@ -1,5 +1,6 @@
-from typing import List, Union, Any, Dict
+from typing import List, Union, Any, Dict, Optional
 from json import loads
+from datetime import datetime
 
 from marshmallow import Schema, fields, post_load
 
@@ -75,3 +76,128 @@ class SubscriptionInput(BaseInput):
     @property
     def vertices(self) -> List["SubscriptionVertexInput"]:
         return self.__vertices
+
+
+# pylint: disable=too-many-instance-attributes
+class NASAFireLocationInput(BaseInput):
+    @classmethod
+    def schema(cls) -> Dict[Any, Any]:
+        return {
+            **super().schema(),
+            "latitude": fields.Float(required=True),
+            "longitude": fields.Float(required=True),
+            "brightness": fields.Float(),
+            "scan": fields.Float(required=True),
+            "track": fields.Float(required=True),
+            "acq_date": fields.Str(required=True),
+            "acq_time": fields.Str(required=True),
+            "satellite": fields.Str(required=True),
+            "confidence": fields.Raw(),
+            "version": fields.Str(required=True),
+            "bright_t31": fields.Float(),
+            "bright_ti4": fields.Float(),
+            "bright_ti5": fields.Float(),
+            "frp": fields.Float(required=True),
+            "daynight": fields.Str(required=True),
+        }
+
+    # pylint: disable=too-many-arguments, too-many-locals
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        scan: float,
+        track: float,
+        acq_date: str,
+        acq_time: str,
+        satellite: str,
+        confidence: Any,
+        version: str,
+        frp: float,
+        daynight: str,
+        brightness: Optional[float] = None,
+        bright_t31: Optional[float] = None,
+        bright_ti4: Optional[float] = None,
+        bright_ti5: Optional[float] = None,
+    ) -> None:
+        super().__init__()
+
+        self.__latitude = latitude
+        self.__longitude = longitude
+        self.__brightness = brightness
+        self.__scan = scan
+        self.__track = track
+        self.__acq_date = acq_date
+        self.__acq_time = acq_time
+        self.__satellite = satellite
+        self.__confidence = confidence
+        self.__version = version
+        self.__frp = frp
+        self.__daynight = daynight
+        self.__bright_t31 = bright_t31
+        self.__bright_ti4 = bright_ti4
+        self.__bright_ti5 = bright_ti5
+
+    @property
+    def latitude(self) -> float:
+        return self.__latitude
+
+    @property
+    def longitude(self) -> float:
+        return self.__longitude
+
+    @property
+    def brightness(self) -> Optional[float]:
+        return self.__brightness
+
+    @property
+    def scan(self) -> float:
+        return self.__scan
+
+    @property
+    def track(self) -> float:
+        return self.__track
+
+    @property
+    def acq_date(self) -> str:
+        return self.__acq_date
+
+    @property
+    def acq_time(self) -> str:
+        return self.__acq_time
+
+    @property
+    def satellite(self) -> str:
+        return self.__satellite
+
+    @property
+    def confidence(self) -> Any:
+        return self.__confidence
+
+    @property
+    def version(self) -> str:
+        return self.__version
+
+    @property
+    def frp(self) -> float:
+        return self.__frp
+
+    @property
+    def daynight(self) -> str:
+        return self.__daynight
+
+    @property
+    def bright_t31(self) -> Optional[float]:
+        return self.__bright_t31
+
+    @property
+    def bright_ti4(self) -> Optional[float]:
+        return self.__bright_ti4
+
+    @property
+    def bright_ti5(self) -> Optional[float]:
+        return self.__bright_ti5
+
+    @property
+    def acquired(self) -> datetime:
+        return datetime.strptime(f"{self.acq_date} {self.acq_time}", "%Y-%m-%d %H%M")
