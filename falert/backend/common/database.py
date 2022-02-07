a0fe82cd4d7e92008b6ece931ef0e1a968546872
Justus Flerlage <justus@sutsuj.com>
2022-02-07 22:20:03 +0100

    Reformat database module

--- falert/backend/common/database.py
+++ falert/backend/common/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
+
 def create_engine() -> AsyncEngine:
     return create_async_engine(
         "sqlite+aiosqlite:///database.db",
